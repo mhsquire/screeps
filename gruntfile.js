ba2c2d40620ba8b3813a7b9beb7c29cbc2a3678f
Matt Squire <mhsquire83@gmail.com>
2022-01-07 13:42:30 -0700

    improvements to gruntfile.js

---
+++ gruntfile.js
@@ -0,0 +1,58 @@
+module.exports = function(grunt) {
+
+    var config = require('./.screeps.json')
+    var branch = grunt.option('branch') || config.branch;
+    var email = grunt.option('email') || config.email;
+    var token = grunt.option('token') || config.token;
+    var ptr = grunt.option('ptr') ? true : config.ptr;
+
+    grunt.loadNpmTasks('grunt-screeps');
+    grunt.loadNpmTasks('grunt-contrib-clean')
+    grunt.loadNpmTasks('grunt-contrib-copy')
+
+    grunt.initConfig({
+        screeps: {
+            options: {
+                email: email,
+                token: token,
+                branch: branch,
+                // ptr: ptr
+            },
+            dist: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'dist/',
+                        src: ['**/*.{js,wasm}'],
+                        flatten: true
+                    }
+                ]
+            },
+
+        },
+        // Remove all files from the dist folder.
+        clean: {
+            'dist': ['dist']
+        },
+
+        // Copy all source files into the dist folder, flattening the folder structure by converting path delimiters to underscores
+        copy: {
+            // Pushes the game code to the dist folder so it can be modified before being send to the screeps server.
+            screeps: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/',
+                    src: '**/*.{js,wasm}',
+                    dest: 'dist/',
+                    filter: 'isFile',
+                    rename: function (dest, src) {
+                        // Change the path name utilize underscores for folders
+                        return dest + src.replace(/\//g,'_');
+                    }
+                }],
+            }
+        },
+    })
+
+    grunt.registerTask('default',  ['clean', 'copy:screeps', 'screeps']);
+}
